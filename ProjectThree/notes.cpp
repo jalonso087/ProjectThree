849b09018beb6078bcec9aa5f9a773bb8bdea85b
Javier Alonso <sicotic87@gmail.com>
2024-06-26 09:59:17 -0700

    Add project files.

---
+++ ProjectThree/notes.cpp
@@ -0,0 +1,38 @@
+/*
+* 
+* 
+	Four menu options
+	Menu option one:
+		prompt the user for the item or word to look for
+		return numeric value for the frequency of the specific word
+	Menu option two
+		Print the list with numbers that represent the frequency of all items purchased.
+		The screen output should include every item (represented by a word) paired with the number of times that item appears in the input file, CS210_Project_Three_Input_File.txt. For example, the file might read as follows:
+			Potatoes 4
+			Pumpkins 5
+			Onions 3
+	Menu option three
+		Print the same frequency information for all the items in the form of a histogram.
+		Then print the name, followed by asterisks or another special character to represent the numeric amount.
+		The number of asterisks should equal the frequency read from the CS210_Project_Three_Input_File.txt file. For example, if the file includes 4 potatoes, 5 pumpkins, and 3 onions, then your text-based histogram may appear as represented below. However, you can alter the appearance or color of the histogram in any way you choose:
+			Potatoes ****
+			Pumpkins *****
+			Onions ***
+	Menu option four:
+		Exit the program
+
+	Data File Creation:
+		Create a data file, with the naming convention frequency.dat, for backing up your accumulated data. The frequency.dat file should include every item (represented by a word) paired with the number of times that item appears in the input file.
+
+		This output file is created in the beginning of the program without user intervention and is for backup purposes. For assistance with writing files, see sections 7.1, "File Input," and 7.3, "File Output," in zyBooks.
+
+	Documentation:
+		Describe your code’s design and functionality. Include screenshots to support your description. As you complete this work, your manager at Chada Tech is interested to see your thought process regarding how you use C++.	
+
+	Optional Challenge: User Input Validation
+		Validating user input is a coding best practice. For an additional challenge with this assignment, use input validation and error handling to anticipate, detect, and respond to run-time and user errors (for example, make sure you have option four on your menu so users can exit the program).
+
+
+	Supporting Materials has a text file to pull data from
+
+*/
